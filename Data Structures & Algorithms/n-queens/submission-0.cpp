bf481e2aae86c5625488c818a45152987e2c83c6
snigdhaha <ep24btech11030@iith.ac.in>
2026-06-12 15:06:26 +0530

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    void solve(int col,
+               vector<string>& board,
+               vector<vector<string>>& ans,
+               vector<int>& leftRow,
+               vector<int>& upperDiag,
+               vector<int>& lowerDiag,
+               int n) {
+
+        if(col == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for(int row = 0; row < n; row++) {
+
+            if(leftRow[row] == 0 &&
+               lowerDiag[row + col] == 0 &&
+               upperDiag[n - 1 + col - row] == 0) {
+
+                board[row][col] = 'Q';
+
+                leftRow[row] = 1;
+                lowerDiag[row + col] = 1;
+                upperDiag[n - 1 + col - row] = 1;
+
+                solve(col + 1, board, ans,
+                      leftRow, upperDiag, lowerDiag, n);
+
+                board[row][col] = '.';
+
+                leftRow[row] = 0;
+                lowerDiag[row + col] = 0;
+                upperDiag[n - 1 + col - row] = 0;
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+
+        vector<string> board(n, string(n, '.'));
+
+        vector<int> leftRow(n, 0);
+        vector<int> upperDiag(2 * n - 1, 0);
+        vector<int> lowerDiag(2 * n - 1, 0);
+
+        solve(0, board, ans,
+              leftRow, upperDiag, lowerDiag, n);
+
+        return ans;
+    }
+};

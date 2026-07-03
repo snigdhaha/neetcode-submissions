5b1b596243481912f215bd6e739eda39ee63c9c1
snigdhaha <ep24btech11030@iith.ac.in>
2026-07-03 11:09:55 +0530

    Add: count-paths - submission-3

---
+++ Data Structures & Algorithms/count-paths/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int f(int i,int j,int m,int n,vector<vector<int>> &dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int left = f(i,j-1,m,n,dp);
+        int up = f(i-1,j,m,n,dp);
+
+        return dp[i][j]=left+up;
+    }
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return f(m-1,n-1,m,n,dp);
+    }
+};
